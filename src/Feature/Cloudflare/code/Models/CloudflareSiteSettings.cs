3eda90ef1856ac11bb61e2397ced32eda2402c1a
Nicole DuRand <Nicole.DuRand@velir.com>
2021-03-06 13:33:55 -0500

    new model for cf settings and stub of publish event

---
+++ src/Feature/Cloudflare/code/Models/CloudflareSiteSettings.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Data.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NerdImmunity2021.Feature.Cloudflare.Models
+{
+    public class CloudflareSiteSettings
+    {
+        public DatasourceField SiteStartItem;
+        public Field DeliveryTargetHostname;
+        public Field CFToken;
+        public Field CFZoneID;
+        public Sitecore.Data.ID SiteStartItemFieldID = new Sitecore.Data.ID("{C62ABB42-B73B-47B8-8422-D2C1CFEB0B23}");
+        public Sitecore.Data.ID DeliveryTargetHostnameFieldID = new Sitecore.Data.ID("{028D2A8B-F42F-4B3E-8B96-A1336A633A9F}");
+        public Sitecore.Data.ID CFTokenFieldID = new Sitecore.Data.ID("{DF80E6F4-CA63-40FD-95E5-2ADC5B995A47}");
+        public Sitecore.Data.ID CFZoneIDFieldID = new Sitecore.Data.ID("{6DFB971D-3517-4CEA-A864-F4CADD369AF3}");
+
+        public CloudflareSiteSettings(Item item)
+        {
+            SiteStartItem = item.Fields[SiteStartItemFieldID];
+            DeliveryTargetHostname = item.Fields[DeliveryTargetHostnameFieldID];
+            CFToken = item.Fields[CFTokenFieldID];
+            CFZoneID = item.Fields[CFZoneIDFieldID];
+        }
+    }
+}
---
+++ src/Feature/Cloudflare/code/Pipelines/CloudflareItemPublishProcessed.cs
@@ -0,0 +1,27 @@
+﻿using NerdImmunity2021.Feature.Cloudflare.Models;
+using Sitecore.Publishing.Pipelines.PublishItem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NerdImmunity2021.Feature.Cloudflare.Pipelines
+{
+    public class CloudflareItemPublishProcessed
+    {
+        public void ItemProcessed(object sender, EventArgs args)
+        {
+            //ItemProcessedEventArgs itemProcessedEventArgs = args as ItemProcessedEventArgs;
+            //PublishItemContext context = itemProcessedEventArgs != null ? itemProcessedEventArgs.Context : null;
+            ////TODO: validate publish is against public database
+            //Sitecore.Data.Items.Item target = context.PublishOptions.TargetDatabase.GetItem(context.ItemId,context.PublishOptions.Language);
+            //if (target == null)
+            //    return;
+            //CloudflarePageSettings publishedPage = new CloudflarePageSettings(target);
+            //if (publishedPage.fullyCachePageField.Checked)
+            //{
+            //    //clear cache
+            //}
+        }
+    }
+}
